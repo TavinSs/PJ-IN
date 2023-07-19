8d7cd1cc2bccd43a394baa9f59467f99f218c454
TavinSs <otaviomdie@gmail.com>
2023-07-19 19:44:39 -0300

    mudo ota vez

--- PJ-IN/TelaBemVindo.Designer.cs
+++ PJ-IN/TelaBemVindo.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TelaBemVindo";
+            label1 = new Label();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(107, 72, 142);
+            label1.Location = new Point(83, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Seja Bem-Vindo";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(107, 72, 142);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(318, 48);
+            panel1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.icons8_excluir_50__1_;
+            pictureBox2.Location = new Point(277, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(29, 31);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // TelaBemVindo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(318, 150);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "TelaBemVindo";
+            Text = "TelaBemVindo";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
     }
 }
