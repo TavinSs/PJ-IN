536af10e0f07936b0221823c03e8f69296ace74c
ALINE DE OLIVEIRA DOS REIS <aline.reis@alunos.sc.senac.br>
2023-07-20 17:41:54 -0300

    modificação na tela evento

--- PJ-IN/TelaEventos.Designer.cs
+++ PJ-IN/TelaEventos.Designer.cs
@@ -28,160 +28,286 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel2 = new Panel();
-            pictureBox4 = new PictureBox();
-            button3 = new Button();
-            pictureBox3 = new PictureBox();
-            button4 = new Button();
-            button2 = new Button();
-            button1 = new Button();
-            panel1 = new Panel();
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaEventos));
+            panel2=new Panel();
+            pictureBox4=new PictureBox();
+            button3=new Button();
+            pictureBox3=new PictureBox();
+            button4=new Button();
+            button2=new Button();
+            button1=new Button();
+            panel1=new Panel();
+            pictureBox1=new PictureBox();
+            pictureBox2=new PictureBox();
+            Evento=new Label();
+            pictureBox5=new PictureBox();
+            pictureBox6=new PictureBox();
+            pictureBox7=new PictureBox();
+            pictureBox8=new PictureBox();
+            label1=new Label();
+            label2=new Label();
+            label3=new Label();
+            label4=new Label();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
             SuspendLayout();
             // 
             // panel2
             // 
-            panel2.BackColor = Color.Gainsboro;
+            panel2.BackColor=Color.Gainsboro;
             panel2.Controls.Add(pictureBox4);
             panel2.Controls.Add(button3);
             panel2.Controls.Add(pictureBox3);
             panel2.Controls.Add(button4);
             panel2.Controls.Add(button2);
             panel2.Controls.Add(button1);
-            panel2.Dock = DockStyle.Left;
-            panel2.Location = new Point(0, 49);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(272, 666);
-            panel2.TabIndex = 16;
+            panel2.Dock=DockStyle.Left;
+            panel2.Location=new Point(0, 65);
+            panel2.Margin=new Padding(3, 4, 3, 4);
+            panel2.Name="panel2";
+            panel2.Size=new Size(311, 888);
+            panel2.TabIndex=16;
             // 
             // pictureBox4
             // 
-            pictureBox4.Image = Properties.Resources.icons8_usuário_homem_com_círculo_48;
-            pictureBox4.Location = new Point(200, 604);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(69, 59);
-            pictureBox4.SizeMode = PictureBoxSizeMode.CenterImage;
-            pictureBox4.TabIndex = 20;
-            pictureBox4.TabStop = false;
+            pictureBox4.Image=Properties.Resources.icons8_usuário_homem_com_círculo_48;
+            pictureBox4.Location=new Point(229, 805);
+            pictureBox4.Margin=new Padding(3, 4, 3, 4);
+            pictureBox4.Name="pictureBox4";
+            pictureBox4.Size=new Size(79, 79);
+            pictureBox4.SizeMode=PictureBoxSizeMode.CenterImage;
+            pictureBox4.TabIndex=20;
+            pictureBox4.TabStop=false;
             // 
             // button3
             // 
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button3.ForeColor = Color.FromArgb(107, 72, 142);
-            button3.Location = new Point(0, 604);
-            button3.Name = "button3";
-            button3.Size = new Size(194, 59);
-            button3.TabIndex = 19;
-            button3.Text = "Sair";
-            button3.UseVisualStyleBackColor = true;
+            button3.FlatAppearance.BorderSize=0;
+            button3.FlatStyle=FlatStyle.Flat;
+            button3.Font=new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor=Color.FromArgb(107, 72, 142);
+            button3.Location=new Point(0, 805);
+            button3.Margin=new Padding(3, 4, 3, 4);
+            button3.Name="button3";
+            button3.Size=new Size(222, 79);
+            button3.TabIndex=19;
+            button3.Text="Sair";
+            button3.UseVisualStyleBackColor=true;
             // 
             // pictureBox3
             // 
-            pictureBox3.BackColor = Color.Gainsboro;
-            pictureBox3.Image = Properties.Resources.logo_removebg_preview;
-            pictureBox3.Location = new Point(0, 0);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(272, 189);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 17;
-            pictureBox3.TabStop = false;
+            pictureBox3.BackColor=Color.Gainsboro;
+            pictureBox3.Image=Properties.Resources.logo_removebg_preview;
+            pictureBox3.Location=new Point(0, 0);
+            pictureBox3.Margin=new Padding(3, 4, 3, 4);
+            pictureBox3.Name="pictureBox3";
+            pictureBox3.Size=new Size(311, 252);
+            pictureBox3.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex=17;
+            pictureBox3.TabStop=false;
             // 
             // button4
             // 
-            button4.FlatAppearance.BorderSize = 0;
-            button4.FlatStyle = FlatStyle.Flat;
-            button4.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button4.ForeColor = Color.FromArgb(107, 72, 142);
-            button4.Location = new Point(3, 341);
-            button4.Name = "button4";
-            button4.Size = new Size(269, 59);
-            button4.TabIndex = 3;
-            button4.Text = "Faturas";
-            button4.UseVisualStyleBackColor = true;
+            button4.FlatAppearance.BorderSize=0;
+            button4.FlatStyle=FlatStyle.Flat;
+            button4.Font=new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor=Color.FromArgb(107, 72, 142);
+            button4.Location=new Point(3, 455);
+            button4.Margin=new Padding(3, 4, 3, 4);
+            button4.Name="button4";
+            button4.Size=new Size(307, 79);
+            button4.TabIndex=3;
+            button4.Text="Faturas";
+            button4.UseVisualStyleBackColor=true;
             // 
             // button2
             // 
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.ForeColor = Color.FromArgb(107, 72, 142);
-            button2.Location = new Point(0, 276);
-            button2.Name = "button2";
-            button2.Size = new Size(272, 59);
-            button2.TabIndex = 1;
-            button2.Text = "Exames";
-            button2.UseVisualStyleBackColor = true;
+            button2.FlatAppearance.BorderSize=0;
+            button2.FlatStyle=FlatStyle.Flat;
+            button2.Font=new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor=Color.FromArgb(107, 72, 142);
+            button2.Location=new Point(0, 368);
+            button2.Margin=new Padding(3, 4, 3, 4);
+            button2.Name="button2";
+            button2.Size=new Size(311, 79);
+            button2.TabIndex=1;
+            button2.Text="Exames";
+            button2.UseVisualStyleBackColor=true;
             // 
             // button1
             // 
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.ForeColor = Color.FromArgb(107, 72, 142);
-            button1.Location = new Point(0, 211);
-            button1.Name = "button1";
-            button1.Size = new Size(272, 59);
-            button1.TabIndex = 0;
-            button1.Text = "Inicio";
-            button1.UseVisualStyleBackColor = true;
+            button1.FlatAppearance.BorderSize=0;
+            button1.FlatStyle=FlatStyle.Flat;
+            button1.Font=new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor=Color.FromArgb(107, 72, 142);
+            button1.Location=new Point(0, 281);
+            button1.Margin=new Padding(3, 4, 3, 4);
+            button1.Name="button1";
+            button1.Size=new Size(311, 79);
+            button1.TabIndex=0;
+            button1.Text="Inicio";
+            button1.UseVisualStyleBackColor=true;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(107, 72, 142);
+            panel1.BackColor=Color.FromArgb(107, 72, 142);
             panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(pictureBox2);
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1300, 49);
-            panel1.TabIndex = 15;
+            panel1.Dock=DockStyle.Top;
+            panel1.Location=new Point(0, 0);
+            panel1.Margin=new Padding(3, 4, 3, 4);
+            panel1.Name="panel1";
+            panel1.Size=new Size(1486, 65);
+            panel1.TabIndex=15;
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.icons8_minimizar_50__1_;
-            pictureBox1.Location = new Point(1224, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(29, 31);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
+            pictureBox1.Image=Properties.Resources.icons8_minimizar_50__1_;
+            pictureBox1.Location=new Point(1399, 16);
+            pictureBox1.Margin=new Padding(3, 4, 3, 4);
+            pictureBox1.Name="pictureBox1";
+            pictureBox1.Size=new Size(33, 41);
+            pictureBox1.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex=1;
+            pictureBox1.TabStop=false;
             // 
             // pictureBox2
             // 
-            pictureBox2.Image = Properties.Resources.icons8_excluir_50__1_;
-            pictureBox2.Location = new Point(1259, 12);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(29, 31);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 0;
-            pictureBox2.TabStop = false;
+            pictureBox2.Image=Properties.Resources.icons8_excluir_50__1_;
+            pictureBox2.Location=new Point(1439, 16);
+            pictureBox2.Margin=new Padding(3, 4, 3, 4);
+            pictureBox2.Name="pictureBox2";
+            pictureBox2.Size=new Size(33, 41);
+            pictureBox2.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex=0;
+            pictureBox2.TabStop=false;
+            // 
+            // Evento
+            // 
+            Evento.AutoSize=true;
+            Evento.Font=new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            Evento.ForeColor=Color.FromArgb(107, 72, 142);
+            Evento.Location=new Point(343, 86);
+            Evento.Name="Evento";
+            Evento.Size=new Size(353, 37);
+            Evento.TabIndex=17;
+            Evento.Text="EVENTOS EM DESTAQUE";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image=Properties.Resources.R;
+            pictureBox5.Location=new Point(343, 155);
+            pictureBox5.Name="pictureBox5";
+            pictureBox5.Size=new Size(183, 236);
+            pictureBox5.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex=18;
+            pictureBox5.TabStop=false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image=Properties.Resources._3;
+            pictureBox6.Location=new Point(919, 164);
+            pictureBox6.Name="pictureBox6";
+            pictureBox6.Size=new Size(183, 236);
+            pictureBox6.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex=19;
+            pictureBox6.TabStop=false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image=Properties.Resources.shutterstock_293337539;
+            pictureBox7.Location=new Point(1190, 164);
+            pictureBox7.Name="pictureBox7";
+            pictureBox7.Size=new Size(183, 236);
+            pictureBox7.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex=20;
+            pictureBox7.TabStop=false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image=(Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location=new Point(614, 155);
+            pictureBox8.Name="pictureBox8";
+            pictureBox8.Size=new Size(183, 236);
+            pictureBox8.SizeMode=PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex=21;
+            pictureBox8.TabStop=false;
+            // 
+            // label1
+            // 
+            label1.AutoSize=true;
+            label1.Location=new Point(357, 394);
+            label1.Name="label1";
+            label1.Size=new Size(147, 20);
+            label1.TabIndex=22;
+            label1.Text="Aniversario do Clube";
+            // 
+            // label2
+            // 
+            label2.AutoSize=true;
+            label2.Location=new Point(640, 394);
+            label2.Name="label2";
+            label2.Size=new Size(131, 20);
+            label2.TabIndex=23;
+            label2.Text="Jogo de  Basquete";
+            // 
+            // label3
+            // 
+            label3.AutoSize=true;
+            label3.Location=new Point(919, 405);
+            label3.Name="label3";
+            label3.Size=new Size(175, 20);
+            label3.TabIndex=24;
+            label3.Text="Campeonato de Voleibol";
+            // 
+            // label4
+            // 
+            label4.AutoSize=true;
+            label4.Location=new Point(1226, 405);
+            label4.Name="label4";
+            label4.Size=new Size(134, 40);
+            label4.TabIndex=25;
+            label4.Text="Aula experimental \r\n     de Natação";
             // 
             // TelaEventos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1300, 715);
+            AutoScaleDimensions=new SizeF(8F, 20F);
+            AutoScaleMode=AutoScaleMode.Font;
+            ClientSize=new Size(1486, 953);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(Evento);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            FormBorderStyle = FormBorderStyle.None;
-            Name = "TelaEventos";
-            Text = "TelaEventos";
+            FormBorderStyle=FormBorderStyle.None;
+            Margin=new Padding(3, 4, 3, 4);
+            Name="TelaEventos";
+            Text="TelaEventos";
             panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -196,5 +322,14 @@
         private PictureBox pictureBox2;
         private PictureBox pictureBox4;
         private Button button3;
+        private Label Evento;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
